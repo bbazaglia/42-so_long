348c9c949cb460c81f1cccce828fd1949a45941b
Beatriz <bbazagli@student.42sp.org.br>
2023-10-19 14:33:28 -0300

    trying to fix hooks

--- src/hook.c
+++ src/hook.c
@@ -18,3 +18,24 @@ void ft_hook(void* param)
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
 		image->instances[0].x += 5;
 } 
+
+// opens and close the window automatically, returning seg fault
+// void ft_hook(void* param)
+// {
+// 	mlx_key_data_t *keydata = NULL;
+// 	mlx_t* mlx = param;
+// 	if (keydata->key == MLX_KEY_ESCAPE && keydata->action == MLX_PRESS)
+// 		mlx_close_window(mlx);
+
+// 	if (keydata->key == MLX_KEY_UP && keydata->action == MLX_PRESS)
+// 		image->instances[0].y -= 5;
+
+// 	if (keydata->key == MLX_KEY_DOWN && keydata->action == MLX_PRESS)
+// 		image->instances[0].y += 5;
+	
+// 	if (keydata->key == MLX_KEY_LEFT && keydata->action == MLX_PRESS)
+// 		image->instances[0].x -= 5;
+
+// 	if (keydata->key == MLX_KEY_RIGHT && keydata->action == MLX_PRESS)
+// 		image->instances[0].x += 5;
+// }
